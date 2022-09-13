2ad0bc7dc9e23b0b76fd2fd2c3e62955868b7832
Meenakshi Lodhi Rajput <rajputmeenakshi982@gmail.com>
2022-09-13 18:03:40 +0530

    project-2

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {default:mongoose} = require('mongoose');
+const route = require('./routes/route');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let url = " ";
+let port = process.env.PORT || 3000;
+
+mongoose.connect(url, {useNewUrlParser: true })
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+
+app.listen(port, function() {
+  console.log("Express app running on port " + port);
+});
+
+
+
+
+
